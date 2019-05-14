981ca30104c09a66d88c981c1e76d479e3b50548
prandtl <vasiliy.uchanev@prandtl.co>
2019-05-15 02:02:49 +0500

    working multiplication

--- automatic-1/matrix.c
+++ automatic-1/matrix.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define X 3
-#define Y 4
-#define Z 5
+#define X 1000
+#define Y 1500
+#define Z 1000
 
-double A[X * Y], B[Y * Z];
+double A[X * Y], B[Y * Z], C[X * Z];
 
 void generate_matrices() {
     int i, j;
@@ -40,10 +40,33 @@ void print_matrice(double M[], int r, int c) {
     }
 }
 
+void multiply_matrices(double A[], double B[], double C[], int x, int y, int z) {
+    int i, j, k;
+    double sum;
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j < z; j++)
+        {
+            sum = 0;
+            for (k = 0; k < y; k++)
+            {
+                sum = sum + A[i * y + k] * B[k * z + j];
+            }
+            C[z * i + j] = sum;
+        }
+    }
+}
+
 void main()
 {
     generate_matrices();
     print_matrice(A, X, Y);
     printf("\n");
     print_matrice(B, Y, Z);
+    printf("\n");
+
+    multiply_matrices(A, B, C, X, Y, Z);
+
+    printf("\n");
+    print_matrice(C, X, Z);
 }
